59db754ea51e6fadfad7de98c1413ca1b4bcc70e
vinicius.cassin <vinicius.cassin@hotmail.com>
2023-04-18 08:05:12 -0300

    feat: create base application with spring initializr

---
+++ transfer-service/src/main/kotlin/com/tm/transfer/TransferApplication.kt
@@ -0,0 +1,11 @@
+package com.tm.services.transfer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TransferApplication
+
+fun main(args: Array<String>) {
+	runApplication<TransferApplication>(*args)
+}
