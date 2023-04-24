05601df6ab11baaa54849e581446ba623888e6ba
vinicius_cassin <vinicius.cassin@hotmail.com>
2023-04-24 08:34:40 -0300

    feat: Add test for validate params of request

--- transfer-service/src/test/kotlin/com/tm/transfer/application/web/controllers/TransferControllerTest.kt
+++ transfer-service/src/test/kotlin/com/tm/transfer/application/web/controllers/TransferControllerTest.kt
@@ -1,8 +1,8 @@
 package com.tm.transfer.application.web.controllers
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.tm.transfer.application.web.entities.requests.TransferRequest
 import com.tm.transfer.builders.buildTransferDataEntity
+import com.tm.transfer.builders.builderTransferRequest
 import com.tm.transfer.domain.repositories.TransferRepository
 import org.junit.Assert.assertEquals
 import org.junit.jupiter.api.Test
@@ -16,7 +16,6 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
-import java.time.LocalDate
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -46,13 +45,7 @@ class TransferControllerTest {
 
     @Test
     fun `Should create transfer succesfully`() {
-        val request = TransferRequest(
-            shippingAccount = 123456,
-            destinationAccount = 54321,
-            value = 1000.0,
-            taxType = "A",
-            scheduleDate = LocalDate.now()
-        )
+        val request = builderTransferRequest()
 
         mockMvc.perform(post("/transfer")
             .contentType(MediaType.APPLICATION_JSON)
